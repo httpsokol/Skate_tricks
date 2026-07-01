32ca0bff1ba646d761b606e8a317911e2f70de59
Wiktor Sokołowski <wiktorsokolowski@MacBook-Air-Wiktor.local>
2026-07-01 23:22:07 +0200

    Gyro plot RT

--- main.py
+++ main.py
@@ -9,22 +9,41 @@ time.sleep(2)
 
 N = 200          # liczba punktów na wykresie
 
-acc_x = deque([0] * N, maxlen=N)    #listy zapisujace poprzednie 200 pomiarow 
+acc_x = deque([0] * N, maxlen=N)    #listy zapisujace poprzednie 200 pomiarow z akcelerometru
 acc_y = deque([0] * N, maxlen=N)
 acc_z = deque([0] * N, maxlen=N)
 
-fig, ax_plot = plt.subplots()
-ax_plot.set_title("ACC")
-ax_plot.set_xlabel("samples")
-ax_plot.set_ylabel("value")
-
-line_x, = ax_plot.plot(range(N), acc_x, label="ax")
-line_y, = ax_plot.plot(range(N), acc_y, label="ay")
-line_z, = ax_plot.plot(range(N), acc_z, label="az")
-
-ax_plot.legend()
+gyro_x = deque([0] * N, maxlen=N)   #listy zapisujace poprzednie 200 pomiarow z gyro
+gyro_y = deque([0] * N, maxlen=N)
+gyro_z = deque([0] * N, maxlen=N)
 
 
+fig, (acc_plot, gyro_plot) = plt.subplots(2, 1)
+
+#ACC plot 
+
+acc_plot.set_title("ACC")
+acc_plot.set_xlabel("samples")
+acc_plot.set_ylabel("value")
+
+line_ax, = acc_plot.plot(range(N), acc_x, label="ax")
+line_ay, = acc_plot.plot(range(N), acc_y, label="ay")
+line_az, = acc_plot.plot(range(N), acc_z, label="az")
+
+acc_plot.legend()
+
+#Gyro plot 
+gyro_plot.set_title("GYRO")
+gyro_plot.set_xlabel("samples")
+gyro_plot.set_ylabel("value")
+
+line_gx, = gyro_plot.plot(range(N), gyro_x, label="gx")
+line_gy, = gyro_plot.plot(range(N), gyro_y, label="gy")
+line_gz, = gyro_plot.plot(range(N), gyro_z, label="gz")
+
+gyro_plot.legend()
+
+# wg matplotlib documentation
 def update(frame):
 
     line = ser.readline().decode("utf-8").strip()
@@ -38,17 +57,25 @@ def update(frame):
     acc_x.append(ax)
     acc_y.append(ay)
     acc_z.append(az)
+    gyro_x.append(gx)
+    gyro_y.append(gy)
+    gyro_z.append(gz)
 
     #aktualizacja wykresu
-    line_x.set_ydata(acc_x)
-    line_y.set_ydata(acc_y)
-    line_z.set_ydata(acc_z)
+    line_ax.set_ydata(acc_x)
+    line_ay.set_ydata(acc_y)
+    line_az.set_ydata(acc_z)
+    line_gx.set_ydata(gyro_x)
+    line_gy.set_ydata(gyro_y)
+    line_gz.set_ydata(gyro_z)
 
     # autoskalowanie wykresu
-    ax_plot.relim()
-    ax_plot.autoscale_view()
+    acc_plot.relim()
+    acc_plot.autoscale_view()
+    gyro_plot.relim()
+    gyro_plot.autoscale_view()
 
-    return line_x, line_y, line_z
+    return line_ax, line_ay, line_az, line_gx, line_gy, line_gz
 
 
 ani = animation.FuncAnimation(fig, update, interval=100)
