b3264b612e36230480e9d9c896a9107848b70880
Wiktor Sokołowski <wiktorsokolowski@MacBook-Air-Wiktor.local>
2026-07-05 00:28:09 +0200

    BMI160 odczyt danych +BLE

--- main.py
+++ main.py
@@ -1,84 +1,24 @@
-import serial
-import time 
-import matplotlib.pyplot as plt 
-import matplotlib.animation as animation
-from collections import deque
+import asyncio
+from bleak import BleakClient
 
-ser = serial.Serial('/dev/cu.usbmodem101', 115200, timeout=1)
-time.sleep(2)
+ADDRESS = "3652F7D3-0006-5771-47AB-439AA4F7E33D"
 
-N = 200          # liczba punktów na wykresie
-
-acc_x = deque([0] * N, maxlen=N)    #listy zapisujace poprzednie 200 pomiarow z akcelerometru
-acc_y = deque([0] * N, maxlen=N)
-acc_z = deque([0] * N, maxlen=N)
-
-gyro_x = deque([0] * N, maxlen=N)   #listy zapisujace poprzednie 200 pomiarow z gyro
-gyro_y = deque([0] * N, maxlen=N)
-gyro_z = deque([0] * N, maxlen=N)
+CHAR_UUID = "87654321-4321-4321-4321-ba0987654321"
 
 
-fig, (acc_plot, gyro_plot) = plt.subplots(2, 1)
-
-#ACC plot 
-
-acc_plot.set_title("ACC")
-acc_plot.set_xlabel("samples")
-acc_plot.set_ylabel("value")
-
-line_ax, = acc_plot.plot(range(N), acc_x, label="ax")
-line_ay, = acc_plot.plot(range(N), acc_y, label="ay")
-line_az, = acc_plot.plot(range(N), acc_z, label="az")
-
-acc_plot.legend()
-
-#Gyro plot 
-gyro_plot.set_title("GYRO")
-gyro_plot.set_xlabel("samples")
-gyro_plot.set_ylabel("value")
-
-line_gx, = gyro_plot.plot(range(N), gyro_x, label="gx")
-line_gy, = gyro_plot.plot(range(N), gyro_y, label="gy")
-line_gz, = gyro_plot.plot(range(N), gyro_z, label="gz")
-
-gyro_plot.legend()
-
-# wg matplotlib documentation
-def update(frame):
-
-    line = ser.readline().decode("utf-8").strip()
-
-    try:
-        ax, ay, az, gx, gy, gz = map(int, line.split(","))
-    except:
-        return
-
-    # dodawanie najnowszych pomiarow do buforu
-    acc_x.append(ax)
-    acc_y.append(ay)
-    acc_z.append(az)
-    gyro_x.append(gx)
-    gyro_y.append(gy)
-    gyro_z.append(gz)
-
-    #aktualizacja wykresu
-    line_ax.set_ydata(acc_x)
-    line_ay.set_ydata(acc_y)
-    line_az.set_ydata(acc_z)
-    line_gx.set_ydata(gyro_x)
-    line_gy.set_ydata(gyro_y)
-    line_gz.set_ydata(gyro_z)
-
-    # autoskalowanie wykresu
-    acc_plot.relim()
-    acc_plot.autoscale_view()
-    gyro_plot.relim()
-    gyro_plot.autoscale_view()
-
-    return line_ax, line_ay, line_az, line_gx, line_gy, line_gz
+def callback(sender, data):
+    print("RX:", data.decode())
 
 
-ani = animation.FuncAnimation(fig, update, interval=100)
+async def main():
+    async with BleakClient(ADDRESS) as client:
 
-plt.tight_layout()
-plt.show()
+        print("Connected:", client.is_connected)
+
+        await client.start_notify(CHAR_UUID, callback)
+
+        while True:
+            await asyncio.sleep(1)
+
+
+asyncio.run(main())
--- src/main.cpp
+++ src/main.cpp
@@ -1,83 +1,102 @@
-#include <Arduino.h> 
-#include <Wire.h>   // I2C 
-#include <DFRobot_BMI160.h>   //BMI160
+#include <Arduino.h>
+#include <Wire.h>
+#include <DFRobot_BMI160.h>
+#include <NimBLEDevice.h>
 
-DFRobot_BMI160 bmi160;    // stworzenie obiektu bmi160 (6-axis gyro)
+DFRobot_BMI160 bmi160;
+NimBLECharacteristic *pCharacteristic;
 
-// fizyczne połączenie ESP32 <-> BMI160
-const int SDA_PIN = 8;  
+const int SDA_PIN = 8;
 const int SCL_PIN = 9;
 
-// tablice odczytow z akcelerometru i zyroskopu [0] -> X | [1] -> Y | [2] -> Z
 int16_t acc[3], gyro[3];
+int16_t acc_offset[3], gyro_offset[3];
 
-// tablice i zmienne pomocnicze do usuniecia offsetu akcelerometru i zyroskopu
-int16_t acc_offset[3], gyro_offset[3]; 
-long acc_offset_x = 0, acc_offset_y = 0, acc_offset_z = 0, gyro_offset_x = 0, gyro_offset_y = 0, gyro_offset_z = 0;
+long acc_offset_x = 0, acc_offset_y = 0, acc_offset_z = 0;
+long gyro_offset_x = 0, gyro_offset_y = 0, gyro_offset_z = 0;
 
-int calibration_samples = 500;
-
-// zmienne wysylane potem do pythona 
-int16_t ax, ay, az, gx, gy, gz;
-
+const int calibration_samples = 300;
 
 void setup() {
-  Serial.begin(115200);   // prędkość komunikacji 115200 bitow na sekunde
-  delay(1000);  // (ms) esp32 potrzebuje czasu, wczesniej wpadał w bootloopa 
+    Serial.begin(115200);
+    delay(1000);
 
-  Wire.begin(SDA_PIN, SCL_PIN);    
+    Wire.begin(SDA_PIN, SCL_PIN);
 
-  //obsluga bledu I2C, gdy wszystko dziala pod adresem 0x69 ma byc zerO!
-  if (bmi160.I2cInit(0x69) != 0) {
-    Serial.println("BMI160 FAIL");
-    while (1);
-  }
+    if (bmi160.I2cInit(0x69) != 0) {
+        Serial.println("BMI160 FAIL");
+        while (1);
+    }
 
-  Serial.println("BMI160 OK");
-  Serial.println("BMI160 Calibrating");
-  for (int i = 0; i < calibration_samples; ++i){
-    bmi160.getAccelData(acc_offset);
-    bmi160.getGyroData(gyro_offset);
-    acc_offset_x += acc_offset[0];
-    acc_offset_y += acc_offset[1];
-    acc_offset_z += acc_offset[2];
-    gyro_offset_x += gyro_offset[0];
-    gyro_offset_y += gyro_offset[1];
-    gyro_offset_z += gyro_offset[2];
-    delay(5);
-  }
-  acc_offset_x /= calibration_samples;
-  acc_offset_y /= calibration_samples;
-  acc_offset_z /= calibration_samples;
-  gyro_offset_x /= calibration_samples;
-  gyro_offset_y /= calibration_samples;
-  gyro_offset_z /= calibration_samples;
-  Serial.println("Done calibrating :)");
+    Serial.println("BMI160 OK - calibrating");
+
+    for (int i = 0; i < calibration_samples; i++) {
+        bmi160.getAccelData(acc_offset);
+        bmi160.getGyroData(gyro_offset);
+
+        acc_offset_x += acc_offset[0];
+        acc_offset_y += acc_offset[1];
+        acc_offset_z += acc_offset[2];
+
+        gyro_offset_x += gyro_offset[0];
+        gyro_offset_y += gyro_offset[1];
+        gyro_offset_z += gyro_offset[2];
+
+        delay(5);
+    }
+
+    acc_offset_x /= calibration_samples;
+    acc_offset_y /= calibration_samples;
+    acc_offset_z /= calibration_samples;
+
+    gyro_offset_x /= calibration_samples;
+    gyro_offset_y /= calibration_samples;
+    gyro_offset_z /= calibration_samples;
+
+    NimBLEDevice::init("ESP32_BMI160");
+
+    NimBLEServer *server = NimBLEDevice::createServer();
+
+    NimBLEService *service =
+        server->createService("12345678-1234-1234-1234-1234567890ab");
+
+    pCharacteristic =
+        service->createCharacteristic(
+            "87654321-4321-4321-4321-ba0987654321",
+            NIMBLE_PROPERTY::READ |
+            NIMBLE_PROPERTY::NOTIFY
+        );
+
+    service->start();
+
+    NimBLEAdvertising *adv = NimBLEDevice::getAdvertising();
+    adv->addServiceUUID(service->getUUID());
+    adv->setName("ESP32_BMI160");
+    adv->enableScanResponse(true);
+
+    NimBLEDevice::startAdvertising();
+
+    Serial.println("BLE READY");
 }
 
 void loop() {
-  // funkcje z biblioteki do BMI160. Odczyt danych i kopia do tablic "acc" i "gyro"
-  bmi160.getAccelData(acc);
-  bmi160.getGyroData(gyro);
+    bmi160.getAccelData(acc);
+    bmi160.getGyroData(gyro);
 
-  ax = acc[0] - acc_offset_x;
-  ay = acc[1] - acc_offset_y;
-  az = acc[2] - acc_offset_z;
-  gx = gyro[0] - gyro_offset_x;
-  gy = gyro[1] - gyro_offset_y;
-  gz = gyro[2] - gyro_offset_z;
+    int16_t ax = acc[0] - acc_offset_x;
+    int16_t ay = acc[1] - acc_offset_y;
+    int16_t az = acc[2] - acc_offset_z;
 
-  
-  Serial.print(ax);
-  Serial.print(",");
-  Serial.print(ay);
-  Serial.print(",");
-  Serial.print(az);
-  Serial.print(",");
-  Serial.print(gx);
-  Serial.print(",");
-  Serial.print(gy);
-  Serial.print(",");
-  Serial.println(gz); // potrzebuje znaku \n, bo readline w python czyta do znaku \n
-  delay(100); 
+    int16_t gx = gyro[0] - gyro_offset_x;
+    int16_t gy = gyro[1] - gyro_offset_y;
+    int16_t gz = gyro[2] - gyro_offset_z;
+
+    char buffer[80];
+    sprintf(buffer, "%d,%d,%d,%d,%d,%d",
+            ax, ay, az, gx, gy, gz);
+
+    pCharacteristic->setValue(buffer);
+    pCharacteristic->notify();
+
+    delay(50); // ~20 Hz na start
 }
